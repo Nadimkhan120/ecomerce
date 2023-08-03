9cbd0bb5b2b3a098f6dc275bb4f7d12287b6ee7c
NadeemKhanFh <124039561+NadeemKhanFh@users.noreply.github.com>
2023-08-03 23:37:10 +0500

    fix(): fix spacings

---
+++ src/screens/products/new-data.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const NewData = () => {
+  return (
+    <View>
+      <Text>NewData</Text>
+    </View>
+  );
+};
+
+export default NewData;
